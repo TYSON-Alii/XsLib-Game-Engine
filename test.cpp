3797e86e56cc740e0bea87a6b3c2f17822690f1e
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2021-09-25 22:54:35 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,6 +9,7 @@
 #include "imgui.h"
 #include "imgui-SFML.h"
 #include "imgui_stdlib.h"
+#include "ImGuizmo.h"
 #define XS_USE_SFML
 #define XS_USE_COLORS
 #define XS_USE_IMGUI
@@ -21,12 +22,24 @@ XsShape v;
 
 int main(int argc, char* argv[]) {
     XsLib engine("oyun motoru");
-    XsVertices plane_vert = XsOBJLoader("data/magic.obj", XS_VERTEX);
-    engine << plane_vert;
-    engine << v;
-    engine << v;
+    XsVertices cube_vert = XsOBJLoader("data/magic.obj", XS_VERTEX);
+    XsVertices kayadam_vert = XsOBJLoader("data/kayadam.obj", XS_VERTEX_AND_TEXTURE);
+    XsTextureSTB kayadam_tex("data/kayadam.png", GL_RGBA);
+    XsVertices mortax_vert = XsOBJLoader("data/mortaks_1.obj", XS_VERTEX_AND_TEXTURE);
+    XsTextureSTB mortax_tex("data/mortaks_1.png", GL_RGBA);
+    XsVertices radxf_mortax_vert = XsOBJLoader("data/radiaktif_mortaks.obj", XS_VERTEX_AND_TEXTURE);
+    XsTextureSTB radxf_mortax_tex("data/radiaktif_mortaks.png", GL_RGBA);
     XsTextureSTB tex("data/pattern32.png");
+    engine << v;
+    engine << v;
     engine << tex;
+    engine << cube_vert;
+    engine << kayadam_vert;
+    engine << mortax_vert;
+    engine << radxf_mortax_vert;
+    engine << kayadam_tex;
+    engine << mortax_tex;
+    engine << radxf_mortax_tex;
 
     engine.Draw();
     return 0;
