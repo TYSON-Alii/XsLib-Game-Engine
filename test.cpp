3ef94d72c50749a33b1e314b798aca0eb832d61e
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2021-10-03 19:00:25 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,36 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define GLEW_ENABLE_EXPERIMENTAL
-#include <GL/glew.h>
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm.hpp>
-#include <gtc/matrix_transform.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/OpenGL.hpp>
-#include "imgui.h"
-#include "imgui-SFML.h"
-#include "imgui_stdlib.h"
-#include "ImGuizmo.h"
-#include "imfilebrowser.h"
-#include "ImGuiFileDialog.h"
-#define XS_USE_SFML
-#define XS_USE_COLORS
-#define XS_USE_IMGUI
-#define XS_USE_GLM
-#include <XsLib.hpp>
-#define WIDTH 1200
-#define HEIGHT 700
-#include "XsEngine.hpp"
+#include <XsEngine.hpp>
 XsShape v;
 
-int main(int argc, char* argv[]) {
+using namespace std;
+
+void myfunc() {
+    if (XsIsKeyPressed(XS_MOUSE_LEFT)) {
+        v.pos.y += 0.1;
+    };
+}
+
+int main() {
     XsLib engine("oyun motoru");
     engine.load("new_proj.xs.save");
     XsVertices cube_vert = XsOBJLoader("data/magic.obj", XS_VERTEX);
     XsTextureSTB tex("data/pattern32.png");
     engine << v;
-    engine << v;
     engine << tex;
     engine << cube_vert;
+    engine.Push(myfunc, "normal");
 
     engine.Start();
     return 0;
